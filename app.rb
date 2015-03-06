a63565e0733d1d900ca4d76258cb5c2fa38d3ae2
jessica <ladyjtm@gmail.com>
2015-03-06 11:54:07 -0800

    Update name of stores and brands. Implement delete fiunction. Edit Styles

--- app.rb
+++ app.rb
@@ -63,3 +63,47 @@ patch "/stores/:id" do
   @brands = Brand.all
   erb(:store_info)
 end
+
+get "/brands/:id/edit" do
+  brand_id = params["id"].to_i
+  @brand = Brand.find(brand_id)
+  erb :brand_edit
+end
+
+get "/stores/:id/edit" do
+  store_id = params["id"].to_i
+  @store = Store.find(store_id)
+  erb :store_edit
+end
+
+patch '/brands/:id/edit' do
+  brand_name = params["brand_name"]
+  brand_id = params["id"].to_i
+  @brand = Brand.find(brand_id)
+  @brand.update({:brand_name => brand_name})
+  @brands = Brand.all
+  erb :brand
+end
+
+patch '/stores/:id/edit' do
+  store_name = params["store_name"]
+  store_id = params["id"].to_i
+  @store = Store.find(store_id)
+  @store.update({:store_name => store_name})
+  @stores = Store.all
+  erb :store
+end
+
+delete '/brands/:id/edit' do
+  brand_id = params["id"].to_i
+  @brand = Brand.find(brand_id)
+  @brand.destroy
+  redirect "/"
+end
+
+delete '/stores/:id/edit' do
+  store_id = params["id"].to_i
+  @store = Store.find(store_id)
+  @store.destroy
+  redirect "/"
+end
